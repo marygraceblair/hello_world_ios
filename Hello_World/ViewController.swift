94d52e971fc057e096cff4b79b5d85f84dc29ffe
MaryGrace Blair <mblair021@gmail.com>
2017-09-19 23:51:27 -0500

    Hello World Base App

--- Hello_World/ViewController.swift
+++ Hello_World/ViewController.swift
@@ -9,16 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //to get this, control-click and drag
+    
+    
+    @IBOutlet weak var helloWorldButton: UIButton!
+    @IBOutlet weak var helloWorldLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.helloWorldLabel.isHidden = true
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func helloWorldButtonClicked(_ sender: Any) {
+        print("HelloWorld")
+        self.helloWorldButton.setTitle("Hello World", for: UIControlState.normal)
+        self.helloWorldLabel.isHidden = false
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
